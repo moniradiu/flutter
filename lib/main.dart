f36735d677fef20edbb475753f0e052c587428aa
moniradiu <moniradiu@gmail.com>
2021-02-11 00:19:50 +0600

    Greadient 3

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
             decoration: BoxDecoration(
               gradient: RadialGradient(
                 colors: [Colors.yellow, Colors.red, Colors.purple],
-                center: Alignment(1.0, 1.0),
+                center: Alignment(0.6, -0.3),
+                focal: Alignment(0.3, -0.1),
+                focalRadius: 1.0,
               ),
             ),
           ),
