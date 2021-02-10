34c808331b27486ea32bede8e35465072f923fa4
moniradiu <moniradiu@gmail.com>
2021-02-11 00:08:02 +0600

    Sweep Grradient

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Radial Gradient Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Gradient Demo'),
-          ),
-          body: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: RadialGradient(
-                  colors: [Colors.yellow, Colors.deepPurple],
-                ),
+      title: 'Flutter  Sweep gradient Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterSweep Demo'),
+        ),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: SweepGradient(
+                colors: [Colors.yellow, Colors.green, Colors.blue],
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
