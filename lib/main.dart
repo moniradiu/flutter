0e3c04a23ca163fd405f2051d922bc5a465b94c3
moniradiu <moniradiu@gmail.com>
2021-02-10 21:31:21 +0600

    23.Gradient color in appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
-// ignore: unused_import
-
-import 'package:first_app/one.dart';
-
-import 'package:first_app/two.dart';
-
 import 'package:flutter/material.dart';
-
-// ignore: unused_import
-import 'lib.dart';
-// ignore: unused_import
-import 'second.dart';
-
-//import 'package:liquid_swipe/Helpers/Helpers.dart';
-//import 'package:liquid_swipe/liquid_swipe.dart';
-// void main() {
-//   runApp(MyApp());
-// }
 
 void main() => runApp(MyApp());
 
@@ -23,46 +6,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              pinned: true,
-              expandedHeight: 200,
-              backgroundColor: Colors.red,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text("Easy Explations"),
-              ),
-            ),
-            SliverList(
-                delegate: SliverChildListDelegate(<Widget>[
-              addDetails("One", "This Is One"),
-              addDetails("Two", "This Is Two"),
-              addDetails("Three", "This Is Three"),
-              addDetails("Four", "This Is Four"),
-              addDetails("Five", "This Is Five"),
-              addDetails("Six", "This Is Six"),
-              addDetails("Seven", "This Is Seven"),
-              addDetails("Eight", "This Is Eight"),
-              addDetails("Nine", "This Is Nine"),
-              addDetails("Ten", "This Is Ten"),
-            ]))
-          ],
-        ),
-      ),
-    );
+        title: 'Flutter Gradient Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Gradient Demo'),
+          ),
+          body: Center(
+              child: Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [Colors.purple, Colors.blue])))),
+        ));
   }
-}
-
-Widget addDetails(
-  String name,
-  String description,
-) {
-  return ListTile(
-    title: Text(name),
-    subtitle: Text(description),
-    leading: CircleAvatar(
-      child: Text(name[0]),
-    ),
-  );
 }
