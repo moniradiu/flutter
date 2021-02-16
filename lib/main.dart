622d9513844b6e14a94a860705ff97f7af7af041
moniradiu <moniradiu@gmail.com>
2021-02-17 01:37:44 +0600

    Image Picker | Load image from camera and gallery

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
+
 import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+
 import 'dart:io';
 
 void main() => runApp(MyApp());
@@ -18,9 +20,31 @@ class MyApp extends StatelessWidget {
 class MyImagePicker extends StatefulWidget {
   @override
   MyImagePickerState createState() => MyImagePickerState();
+
+  void setState(Null Function() param0) {}
 }
 
 class MyImagePickerState extends State {
+  File _image, _gallery;
+
+  Future CameraImage() async {
+    // ignore: non_constant_identifier_names
+    var Image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = Image;
+    });
+  }
+
+  Future GalleryImage() async {
+    // ignore: non_constant_identifier_names
+    var Gallery = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _gallery = Gallery;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,20 +54,27 @@ class MyImagePickerState extends State {
           height: 500,
           width: double.infinity,
           color: Colors.blue,
+          child: _image == null
+              ? Center(child: Text("No Image Selected"))
+              : Image.file(_image),
         ),
         Divider(),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                CameraImage();
+              },
               child: Icon(Icons.camera),
             ),
             SizedBox(
               width: 20,
             ),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                GalleryImage();
+              },
               child: Icon(Icons.photo_library),
             ),
           ],
